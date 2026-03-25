7f94ca184b0e02f0fc8d2d7094fa32e5c62916b1
ellie-2026 <elisa1.ferri@mail.polimi.it>
2026-03-25 17:40:36 +0100

    clickscounter

---
+++ src/lib/components/ClicksCounter.svelte
@@ -0,0 +1,15 @@
+<script>
+ console.log('Hello from SvelteKit!');   
+ let count = $state(2);
+ let doubled = $derived(count * 2);
+</script>
+
+<h1>Click Counter</h1>
+
+<button onclick={() => count++}>
+    clicks: {count}
+</button>
+
+<span>
+    doubled: {doubled}
+</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,30 @@
 <script>
-console.log("Hello World");
+ console.log('Hello from SvelteKit!');   
+ let count = $state(2);
+ let doubled = $derived(count * 2);
+ import ClicksCounter from "$lib/components/ClicksCounter.svelte"
+ import ProgressiveSum from "$lib/components/ProgressiveSum.svelte"
 </script>
+
+
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+
+<button onclick={() => count++}>
+    clicks: {count}
+</button>
+
+<span>
+    doubled: {doubled}
+</span>
+
 <style>
-    h1{
-        color:chartreuse;
-    }
-    p{
-        color:blueviolet;
+    h1 {
+        color: deepskyblue;
     }
 </style>
+
